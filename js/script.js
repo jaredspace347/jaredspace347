c9b8c8e0a919dee988be0db991e35dd9a3ee8307
jaredspace347 <125144137+jaredspace347@users.noreply.github.com>
2023-02-11 23:12:31 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//Toggle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// ketika hamburger menu di klik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// klik diluar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+// NAMA  : MUHAMMAD IRFAN DWI PUTRA
+// NIM   : 10122345
+// KELAS : IF-9
